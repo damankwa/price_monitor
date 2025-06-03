0d7e85b37f4d81be26834da6d04641d075f0b1c9
David Amankwa <davidamankwa@gmail.com>
2025-06-03 18:46:36 -0400

    Update TestController.java

--- src/main/java/com/damankwa/price_monitor/controller/TestController.java
+++ src/main/java/com/damankwa/price_monitor/controller/TestController.java
@@ -1,13 +1,17 @@
 package com.damankwa.price_monitor.controller;
 
-import com.damankwa.price_monitor.entity.MonitoredProduct;
-import com.damankwa.price_monitor.repository.MonitoredProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.damankwa.price_monitor.entity.MonitoredProduct;
+import com.damankwa.price_monitor.repository.MonitoredProductRepository;
 
 @RestController
 @RequestMapping("/api/test")
@@ -34,8 +38,8 @@ public class TestController {
     @PostMapping("/products")
     public MonitoredProduct createTestProduct() {
         MonitoredProduct product = new MonitoredProduct(
-            "Test iPhone", 
-            "https://example.com/iphone", 
+            "Test Motorola", 
+            "https://example.com/motorola", 
             "test@example.com"
         );
         product.setCurrentPrice(new BigDecimal("999.99"));
